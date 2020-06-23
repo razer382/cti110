462c71f27efd4cd068d8417851f3fabdcda02d38
razer382 <67071120+razer382@users.noreply.github.com>
2020-06-23 18:01:59 -0400

    Add files via upload

---
+++ P3LAB_Debugging_BrandenAlder.py
@@ -0,0 +1,17 @@
+#CTI-110
+#P3LAB - Debugging
+#Branden Alder
+#6/23/2020
+#
+
+
+if score >= A_score:
+    print('Your grade is A. ')
+elif score >= B_score:
+    print('Your grade is B. ')
+elif score >= C_score:
+    print('Your grade is C. ')
+elif score >= D_score:
+    print('Your grade is D. ')
+else:
+    print('Your grade is F. ')
